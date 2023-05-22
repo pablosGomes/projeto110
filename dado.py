09bef5a52dce9bdd8408877066109e6658419b3c
pablosGomes <104365552+pablosGomes@users.noreply.github.com>
2023-05-22 20:04:08 -0300

    Add files via upload

---
+++ dado.py
@@ -0,0 +1,31 @@
+import random
+from urllib import response
+
+response = input("você quer jogar um dado?")
+
+response = "y"
+
+response = "n"
+
+if (response == "y"):
+    no = random.randint(1.6)
+    print(no)
+    if (response == "n"):
+
+
+if no == 1:
+    print("[-----]")
+    print("[     ]")
+    print("[  0  ]")
+    print("[     ]")
+    print("[-----]")
+
+
+
+
+
+
+
+
+
+
